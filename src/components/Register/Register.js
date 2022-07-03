a9dbe16856cc10e29d1ce6121ef07c5c79933410
alamin <mh1134938@gmail.com>
2022-07-03 13:28:25 +0600

    header function

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,21 +1,21 @@
 import React from "react";
+import "./Register.css";
 
-const Login = () => {
+const Register = () => {
   return (
     <div>
-      <h1>Please Login</h1>
-      <div style={{ margin: "20px" }}>
-        <button>Google Sign In</button>
-      </div>
+      <h2>Please Register</h2>
       <form>
+        <input type="text" placeholder="Your Name" />
+        <br />
         <input type="email" name="" id="" placeholder="Your Email" />
         <br />
         <input type="password" name="" id="" placeholder="Your Password" />
         <br />
-        <input type="submit" value="Login" />
+        <input type="submit" value="Register" />
       </form>
     </div>
   );
 };
 
-export default Login;
+export default Register;
