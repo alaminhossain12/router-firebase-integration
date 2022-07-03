e0b9c34229d12e2c7ccf504b4117216ab9529160
alamin <mh1134938@gmail.com>
2022-07-03 13:02:11 +0600

    project setup commit

---
+++ src/firebase.init.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAQm4YXGcJyi-YRor-iUp6lX2Aq58AKyJU",
+  authDomain: "react-router-integration-ae418.firebaseapp.com",
+  projectId: "react-router-integration-ae418",
+  storageBucket: "react-router-integration-ae418.appspot.com",
+  messagingSenderId: "327085761376",
+  appId: "1:327085761376:web:50223e2d9d4726e9a43405",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
